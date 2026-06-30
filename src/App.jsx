b157a972cae8c917a3d2c4d0411af8504e8ae6a5
Alek0MC2009 <alejandrobravolingyte@gmail.com>
2026-06-30 13:06:32 +0200

    Removed i18n and sorted the components, added the demo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import LandingPage from './components/common/landing-page'
-import { Routes, Route } from 'react-router-dom'
+import LandingPage from './components/pages/landing-page'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { DemoPage } from './components/pages/demo-page'
 function App() {
   return (
     <Routes>
-   
-      <Route path="/" element={<LandingPage />}/>
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<LandingPage />} />
+      <Route path="/demo-download" element={<DemoPage />} />
     </Routes>
   )
 }
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -1,28 +1,39 @@
-import { useState } from 'react'
-import { Button } from './button'
-import { englishTexts, spanishTexts, getCurrentLang } from '@/data/texts'
+//import { useState } from 'react'
+// import { Button } from './button'
+// import { englishTexts, spanishTexts, getCurrentLang } from '@/data/texts'
+import { Link } from 'react-router-dom'
 export default function Header() {
-  const [language, setLanguage] = useState('en')
-
-  const texts = getCurrentLang(language)
-
-  const toggleLanguage = () => {
-    setLanguage((prev) => (prev === 'en' ? 'es' : 'en'))
-  }
-
   return (
     <header className="bg-gray-700 text-white font-pixel text-3xl">
       <div className="flex justify-between">
         <ul className="flex gap-4 text-center w-screen justify-between p-2 mx-4 my-2">
-          {texts.navHeader.map((item) => (
-            <li key={item} className="cursor-pointer">
-              <a href="#">{item}</a>
-            </li>
-          ))}
-
-          <Button onClick={toggleLanguage}>
-            {language === 'en' ? '🇪🇸' : '🇬🇧'}
-          </Button>
+          <li>
+            <Link
+              className="text-3xl hover:text-4xl hover:text-cyan-600"
+              to="/"
+            >
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="text-3xl hover:text-4xl hover:text-cyan-600"
+              to="/demo-download"
+            >
+              DEMO
+            </Link>
+          </li>
+          <li>
+            <Link className="text-3xl hover:text-4xl hover:text-cyan-600" to="">
+              TEAM
+            </Link>
+          </li>
+          <li>
+            <Link className="text-3xl hover:text-4xl hover:text-cyan-600" to="">
+              CONTACT
+            </Link>
+          </li>
+          {/* Eliminamos el i18n por que es demasiado trabajo en una version */}
         </ul>
       </div>
     </header>
