2e82653942fbe7ab8a314dcce5343ce8a89a13a4
Alek0MC2009 <alejandrobravolingyte@gmail.com>
2026-07-01 13:26:19 +0200

    1r proyecto daw

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import LandingPage from './components/pages/landing-page'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { DemoPage } from './components/pages/demo-page'
-import TeamPage from './components/pages/team-page'
+// import { DemoPage } from './components/pages/demo-page'
+// import TeamPage from './components/pages/team-page'
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/home" element={<LandingPage />} />
-      <Route path="/demo-download" element={<DemoPage />} />
-      <Route path="/team-page" element={<TeamPage />} />
     </Routes>
   )
 }
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -1,44 +1,7 @@
-//import { useState } from 'react'
-// import { Button } from './button'
-// import { englishTexts, spanishTexts, getCurrentLang } from '@/data/texts'
-import { Link } from 'react-router-dom'
-export default function Header() {
-  return (
-    <header className="bg-gray-700 text-white font-pixel text-3xl">
-      <div className="flex justify-between">
-        <ul className="flex gap-4 text-center w-screen justify-between p-2 mx-4 my-2">
-          <li>
-            <Link
-              className="text-3xl hover:text-4xl hover:text-cyan-600"
-              to="/"
-            >
-              ABOUT
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-3xl hover:text-4xl hover:text-cyan-600"
-              to="/demo-download"
-            >
-              DEMO
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-3xl hover:text-4xl hover:text-cyan-600"
-              to="/team-page"
-            >
-              TEAM
-            </Link>
-          </li>
-          <li>
-            <Link className="text-3xl hover:text-4xl hover:text-cyan-600" to="">
-              CONTACT
-            </Link>
-          </li>
-          {/* Eliminamos el i18n por que es demasiado trabajo en una version */}
-        </ul>
-      </div>
-    </header>
-  )
+import React from 'react'
+
+function Header() {
+  return <div></div>
 }
+
+export default Header
